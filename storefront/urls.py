4c905f758d849597762a407f0614ff0c7ea1a671
amanueldemirew <amanuel.demirew@yahoo.com>
2023-11-08 19:14:32 +0300

    storefront modification

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,7 +1,8 @@
-"""storefront URL Configuration
+"""
+URL configuration for storefront project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("playground/", include("playground.urls")),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
