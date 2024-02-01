44a5a54f8a31ebdd97cda7dd2ba878bf379e6b44
amanueldemirew <amanuel.demirew@yahoo.com>
2024-02-01 16:29:04 +0300

    serializeer

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework'
+    'rest_framework',
     "playground",
     "debug_toolbar",
     "store",
@@ -139,3 +139,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
